92bc848bc61646134680c47851f4258c496a791a
mfigueroa <monserrat.figueroa@sansano.usm.cl>
2019-06-05 01:34:56 -0400

    Gráfico y websocket

---
+++ serverPrueba.js
@@ -0,0 +1,41 @@
+  // request is just an enhanced HTTP request. For more info 
+var Server = require('ws').Server;
+var port = process.env.PORT || 3000;
+var ws = new Server({port: 3000});
+
+var sockets = [];
+var a;
+ws.on('connection', function(w){
+  console.log('connection');
+  
+  w.send('0');
+  w.on('message', function(msg){
+    var id = w.upgradeReq.headers['sec-websocket-key'];
+    var message = JSON.parse(msg);
+    
+    sockets[message.to].send(message.message);
+
+    console.log('Message on :: ', id);
+    console.log('On message :: ', msg);
+  });
+  
+  w.on('close', function() {
+    var id = w.upgradeReq.headers['sec-websocket-key'];
+    console.log('Closing :: ', id);
+  });
+
+
+  for (var i = 1; i <= 100; i++) {
+    w.send(i);
+    a=sleep(10);
+  }
+
+});
+
+function msleep(n) {
+  Atomics.wait(new Int32Array(new SharedArrayBuffer(4)), 0, 0, n);
+}
+function sleep(n) {
+  msleep(n*1000);
+}
+

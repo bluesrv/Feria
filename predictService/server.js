9cfc5c04409c712aacd5ba5eb219a281e1417a94
Ciamar <florencia.cuzmar@sansano.usm.cl>
2019-06-06 07:52:50 -0400

    process and predict audio

--- predictService/server.js
+++ predictService/server.js
@@ -1,37 +1,122 @@
-// Require the framework and instantiate it
-const fastify = require('fastify')({ logger: true })
+'use strict'
 
-// Declare a route
-fastify.get('/', async (request, reply) => {
-  return { hello: 'world' }
+const fastify = require('fastify')()
+const tf = require("@tensorflow/tfjs-node");
+let url = require('url')
+let http = require('http')
+const fs = require('fs')
+var wav = require('wav');
+var EventEmitter = require("events").EventEmitter
+var prediction = new EventEmitter();
+var Meyda = require("meyda")
+const Stream = require('stream')
+const request = require('request')
+
+
+function convertBlock(incomingData) { // incoming data is a UInt8Array
+    var i, l = incomingData.length;
+    var outputData = new Float32Array(incomingData.length);
+    for (i = 0; i < l; i++) {
+        outputData[i] = (incomingData[i] - 128) / 128.0;
+    }
+    return outputData;
+}
+
+async function predict() {
+
+  const model = await tf.loadLayersModel('file://model/model.json');
+
+  var tWindow = 1764
+  var count = 0
+  var dataArray = []
+  var frame = []
+
+  var stream = new Stream.Readable({
+    read(size){
+      stream.pause()
+    }
+  })
+
+  stream.on('readable', () => {
+    let chunk;
+    while (null !== (chunk = stream.read(tWindow))) {
+
+      var ta = convertBlock(chunk)
+      var array =  Array.prototype.slice.call(ta)
+      array = array.concat(new Array(284).fill(0))
+      Meyda.numberOfMFCCCoefficients = 22;
+      var coefs = Meyda.extract('mfcc', array)
+      if (count == 25) {
+        count = 0
+        readable.push(dataArray)
+        dataArray = []
+
+      } else {
+        count += 1
+        dataArray.push(coefs)
+      }
+
+  }
+
 })
 
-// Connect to streaming
+  var readable = new Stream.Readable({objectMode: true,
+    read(size) {
+      readable.pause()
+    }}
+  )
 
-function camera(){
-  //Camera Authentication
-  var ip_address = "192.168.1.110"
-  //camera username and password
-  var username = "feria";
-  var password="feria2019";
-  var port='554'
-
-  //A channel of camera stream
-  Stream = require('node-rtsp-stream');
-  stream = new Stream({
-      streamUrl: 'rtsp://'+username+':'+password+'@'+ip_address+':'+port+'/videoSub',
-      wsPort: 9999    
+  readable.on('readable', () => {
+    let chunk;
+    while (null !== (chunk = readable.read())) {
+      var wrap = []
+      wrap.push(chunk)
+      wrap = tf.tensor(wrap)
+      var tensorData = model.predict(wrap).dataSync()
+      console.log(tensorData[0])
+      prediction.emit('newPrediction', tensorData[0])
+    }
   });
-}
 
-// Run the server!
-const start = async () => {
-  try {
-    await fastify.listen(3000)
-    fastify.log.info(`server listening on ${fastify.server.address().port}`)
-  } catch (err) {
-    fastify.log.error(err)
-    process.exit(1)
+  const myWritable = new Stream.Writable({
+    write(chunk, encoding, next) {
+      stream.push(chunk)
+      next()
+    }
+  });
+
+  var reader = new wav.Reader();
+  reader.on('format', function (format) {
+    reader.pipe(myWritable);
+  });
+
+  request('http://192.168.100.74:8080/audio.wav').pipe(reader)
+
   }
-}
-start()
+
+fastify.register(require('fastify-ws'))
+
+//iniciar el servidor
+fastify.ready(err => {
+  if (err) throw err
+
+  console.log('Server started.')
+  predict()
+
+  fastify.ws.on('connection', (socket, request) => {
+      console.log('Client connected.')
+
+      let sendPrediction = function(res) {
+        socket.send(res)
+      }
+
+      prediction.on('newPrediction', sendPrediction)
+
+      socket.on('close', () => {
+        prediction.removeListener("newPrediction", sendPrediction)
+        console.log('Client disconnected.')
+      })
+    })
+})
+
+fastify.listen(34567)

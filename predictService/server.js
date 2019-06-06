5b9b1a90ecbd4021afa30bf7d25b650cda5f7ac6
Jorge Contreras <jorge.contrerasc@sansano.usm.cl>
2019-06-06 02:39:58 -0400

    previsional requirement

--- predictService/server.js
+++ predictService/server.js
@@ -6,6 +6,24 @@ fastify.get('/', async (request, reply) => {
   return { hello: 'world' }
 })
 
+// Connect to streaming
+
+function camera(){
+  //Camera Authentication
+  var ip_address = "192.168.1.110"
+  //camera username and password
+  var username = "feria";
+  var password="feria2019";
+  var port='554'
+
+  //A channel of camera stream
+  Stream = require('node-rtsp-stream');
+  stream = new Stream({
+      streamUrl: 'rtsp://'+username+':'+password+'@'+ip_address+':'+port+'/videoSub',
+      wsPort: 9999    
+  });
+}
+
 // Run the server!
 const start = async () => {
   try {

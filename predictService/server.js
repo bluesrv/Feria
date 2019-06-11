cefa381e8dd6f20e484d2f6ce5961153fd67f847
Ciamar <florencia.cuzmar@sansano.usm.cl>
2019-06-11 03:26:39 -0400

    nuevo modelo

--- predictService/server.js
+++ predictService/server.js
@@ -13,11 +13,11 @@ const Stream = require('stream')
 const request = require('request')
 
 
-function convertBlock(incomingData) { // incoming data is a UInt8Array
+function convertBlock(incomingData, n) { // incoming data is a UInt8Array
     var i, l = incomingData.length;
     var outputData = new Float32Array(incomingData.length);
     for (i = 0; i < l; i++) {
-        outputData[i] = (incomingData[i] - 128) / 128.0;
+        outputData[i] = (incomingData[i]) / ((2**n)/1.0);
     }
     return outputData;
 }
@@ -32,7 +32,7 @@ async function predict() {
 
   const model = await tf.loadLayersModel('file://model/model.json');
 
-  var tWindow = 1764
+  var tWindow = 512
   var count = 0
   var dataArray = []
   var frame = []
@@ -43,24 +43,29 @@ async function predict() {
     }
   })
 
+  var aux = 0;
   stream.on('readable', () => {
     let chunk;
     while (null !== (chunk = stream.read(tWindow*2))) {
+      var ta = convertTypedArray(chunk, Int16Array)
+      ta = convertBlock(ta, 16)
 
-      var ta = convertTypedArray(chunk, Uint16Array)
-      var ta = convertBlock(ta)
-      var array =  Array.prototype.slice.call(ta)
-      array = array.concat(new Array(284).fill(0))
-      Meyda.numberOfMFCCCoefficients = 22;
-      var coefs = Meyda.extract('mfcc', array)
-      if (count == 25) {
+      Meyda.numberOfMFCCCoefficients = 25;
+      var coefs = Meyda.extract('mfcc', ta)
+
+      var sum = 0
+      for (var i = 0; i < coefs.length; i++) {
+        sum += coefs[i]
+      }
+
+      dataArray.push([sum/25])
+      if (count == 172) {
         count = 0
         readable.push(dataArray)
         dataArray = []
 
       } else {
         count += 1
-        dataArray.push(coefs)
       }
 
   }
@@ -76,11 +81,14 @@ async function predict() {
   readable.on('readable', () => {
     let chunk;
     while (null !== (chunk = readable.read())) {
+      console.log(chunk)
+
       var wrap = []
       wrap.push(chunk)
       wrap = tf.tensor(wrap)
-      var tensorData = model.predict(wrap).dataSync()
+      var tensorData = model.predict(wrap)
       console.log(tensorData[0])
+
       prediction.emit('newPrediction', tensorData[0])
     }
   });
@@ -92,12 +100,20 @@ async function predict() {
     }
   });
 
+  var wav = require('wav');
   var reader = new wav.Reader();
+
   reader.on('format', function (format) {
+    console.log(format)
     reader.pipe(myWritable);
   });
 
-  request('http://10.11.3.133:8080/audio.wav').pipe(reader)
+  var ffmpeg = require('fluent-ffmpeg')
+  ffmpeg('audio1.wav')
+    .audioChannels(1)
+    .format('wav')
+    .pipe(reader)
+
 
   }
 

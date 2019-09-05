968155880aaa470399ed6e2267b445bf0ab6eea1
mfigueroa <monserrat.figueroa@sansano.usm.cl>
2019-09-05 11:27:25 -0400

    browser notification
    notificaciones de Chrome

--- frontService/menu/urls.py
+++ frontService/menu/urls.py
@@ -1,4 +1,5 @@
 from django.urls import path
+from django.views.generic import TemplateView
 
 from menu.views import index,camara,registros,login, camaraU,indexU
 urlpatterns = [
@@ -8,5 +9,6 @@ urlpatterns = [
     path('login',login,name="login"),
     path('camaraU',camaraU,name="camaraU"),
     path('indexU',indexU,name="indexU"),
+    path('sw.js', (TemplateView.as_view(template_name="sw.js", content_type='application/javascript', )), name='sw.js'),
 
  ]

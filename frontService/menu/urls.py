e43ea9e27db1ba20adf6fe972779c44e65bd60dd
mfigueroa <monserrat.figueroa@sansano.usm.cl>
2019-09-05 03:14:17 -0400

    login mockup
    login y vistas de usuario

--- frontService/menu/urls.py
+++ frontService/menu/urls.py
@@ -1,9 +1,12 @@
 from django.urls import path
 
-from.import views
-from menu.views import index,camara,registros
+from menu.views import index,camara,registros,login, camaraU,indexU
 urlpatterns = [
     path('', index, name='index'),
     path('camara',camara, name='camara'),
     path('registros',registros, name="registros"),
+    path('login',login,name="login"),
+    path('camaraU',camaraU,name="camaraU"),
+    path('indexU',indexU,name="indexU"),
+
  ]
--- frontService/menu/views.py
+++ frontService/menu/views.py
@@ -9,3 +9,9 @@ def camara(request):
 	return render(request,'menu/camara.html')
 def registros(request):
 	return render(request,'menu/registros.html')
+def login(request):
+	return render(request,'base/login.html')
+def indexU(request):
+	return render(request,'menu/indexU.html')
+def camaraU(request):
+	return render(request,'menu/camaraU.html')

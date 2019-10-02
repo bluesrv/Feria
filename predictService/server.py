505baccf9fa36b5791a1e164077f9513b4a36259
Ciamar <florencia.cuzmar@sansano.usm.cl>
2019-10-02 23:44:56 +0200

    model feedback

--- predictService/server.py
+++ predictService/server.py
@@ -5,7 +5,7 @@ import numpy as np
 import read_data
 
 HOST = '127.0.0.1'  # Standard loopback interface address (localhost)
-PORT = 3000        # Port to listen on (non-privileged ports are > 1023)
+PORT = 65432        # Port to listen on (non-privileged ports are > 1023)
 
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
     s.bind((HOST, PORT))
